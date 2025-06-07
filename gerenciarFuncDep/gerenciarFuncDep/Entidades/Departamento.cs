b301457307768e11680becc297cd5228deb46c12
Tawan Vitor <155584979+TawanVitor@users.noreply.github.com>
2025-06-07 17:27:30 -0300

    Add files via upload

---
+++ gerenciarFuncDep/gerenciarFuncDep/Entidades/Departamento.cs
@@ -0,0 +1,6 @@
+public class Departamento : IEntidade
+{
+    public Guid Id { get; set; }
+    public string NomeDepartamento { get; set; }
+    public string Sigla { get; set; }
+}
---
+++ gerenciarFuncDep/gerenciarFuncDep/Entidades/Funcionario.cs
@@ -0,0 +1,7 @@
+public class Funcionario : IEntidade
+{
+    public Guid Id { get; set; }
+    public string NomeCompleto { get; set; }
+    public string Cargo { get; set; }
+    public Guid DepartamentoId { get; set; }
+}
