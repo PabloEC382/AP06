fee40e998b254b76b4b9605dbe662d0fe7391f22
Tawan Vitor <155584979+TawanVitor@users.noreply.github.com>
2025-06-07 17:25:23 -0300

    Update Program.cs

--- gerenciarFuncDep/gerenciarFuncDep/Program.cs
+++ gerenciarFuncDep/gerenciarFuncDep/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        var repoDepartamento = new GenericJsonRepository<Departamento>("departamentos.json");
+        var repoFuncionario = new GenericJsonRepository<Funcionario>("funcionarios.json");
+
+        Console.Write("Digite o nome do departamento: ");
+        string nomeDepartamento = Console.ReadLine();
+        Console.Write("Digite a sigla do departamento: ");
+        string sigla = Console.ReadLine();
+
+        var departamento = new Departamento
+        {
+            NomeDepartamento = nomeDepartamento,
+            Sigla = sigla
+        };
+        repoDepartamento.Adicionar(departamento);
+
+        Console.Write("Digite o nome do funcionário: ");
+        string nomeFuncionario = Console.ReadLine();
+        Console.Write("Digite o cargo: ");
+        string cargo = Console.ReadLine();
+
+        var funcionario = new Funcionario
+        {
+            NomeCompleto = nomeFuncionario,
+            Cargo = cargo,
+            DepartamentoId = departamento.Id
+        };
+        repoFuncionario.Adicionar(funcionario);
+
+        Console.WriteLine("
+Funcionários cadastrados:");
+        foreach (var f in repoFuncionario.ObterTodos())
+        {
+            var depto = repoDepartamento.ObterPorId(f.DepartamentoId);
+            Console.WriteLine($"{f.NomeCompleto} - {f.Cargo} - Departamento: {depto?.NomeDepartamento} ({depto?.Sigla})");
+        }
+    }
+}
