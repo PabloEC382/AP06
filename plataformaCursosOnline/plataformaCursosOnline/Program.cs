0046aa3d5c0c54560b03c22dd45a5955d38d144b
Tawan Vitor <155584979+TawanVitor@users.noreply.github.com>
2025-06-08 14:51:09 -0300

    Update Program.cs

--- plataformaCursosOnline/plataformaCursosOnline/Program.cs
+++ plataformaCursosOnline/plataformaCursosOnline/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using Entidades;
+using Implementacoes;
+
+class Program
+{
+    static void Main()
+    {
+        var repositorio = new CursoOnlineJsonRepository();
+        var servico = new CatalogoCursosService(repositorio);
+
+        var curso = new CursoOnline
+        {
+            Titulo = "C# com Repository Pattern",
+            Instrutor = "João Silva",
+            Descricao = "Curso prático sobre o padrão Repository com persistência em JSON.",
+            CargaHorariaHoras = 12
+        };
+
+        if (servico.RegistrarCurso(curso))
+            Console.WriteLine("Curso registrado com sucesso!");
+        else
+            Console.WriteLine("Curso já existe!");
+
+        Console.WriteLine("\nCursos disponíveis:");
+        foreach (var c in servico.ListarCursos())
+        {
+            Console.WriteLine($"- {c.Titulo} ({c.CargaHorariaHoras}h)");
+        }
+    }
+}
